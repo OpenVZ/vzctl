787fe17610ca25e2496f8f44a00109fdeab4f094
Igor Sukhih <igor@parallels.com>
2016-01-26 16:36:46 +0300

    Fixed compilation
    vzctl.c:177:43: error: 'VZCTL_PARAM_MEMGUARANTEE' undeclared here (not in a function)
      {"memguarantee",required_argument, NULL, VZCTL_PARAM_MEMGUARANTEE},
    
    Signed-off-by: Igor Sukhih <igor@parallels.com>

--- src/vzctl.c
+++ src/vzctl.c
@@ -174,7 +174,7 @@ static struct option set_options[] =
 	{"totvmpages",	required_argument, NULL, VZCTL_PARAM_TOTVMPAGES},
 	{"memory",	required_argument, NULL, VZCTL_PARAM_MEMORY},
 		{"ram", required_argument, NULL, VZCTL_PARAM_MEMORY},
-	{"memguarantee",required_argument, NULL, VZCTL_PARAM_MEMGUARANTEE},
+	{"memguarantee",required_argument, NULL, VZCTL_PARAM_MEM_GUARANTEE},
 	{"vm_overcommit",  required_argument, NULL, VZCTL_PARAM_VM_OVERCOMMIT},
 /* CPU */
 	{"cpuweight",	required_argument, NULL, VZCTL_PARAM_CPUWEIGHT},
