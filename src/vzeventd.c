156d0bcb56d97572ce6855624e4d5478700b8cb9
Igor Sukhih <igor@virtuozzo.com>
2017-07-25 15:52:42 +0300

    vzeventd: process ext4 ABORT uevent to trigger fserror #PSBM-68848
    New event was introduced in vzkernel > rh7-3.10.0-514.26.1.vz7.33.16
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- src/vzeventd.c
+++ src/vzeventd.c
@@ -213,7 +213,7 @@ static void process_uevent(void)
 	udev_list_entry_foreach(e, udev_device_get_properties_list_entry(udev)) {
 		n = udev_list_entry_get_name(e);
 		v = udev_list_entry_get_value(e);
-		if (!strcmp(n, "FS_ACTION") && !strcmp(v, "ERROR"))
+		if (!strcmp(n, "FS_ACTION") && !strcmp(v, "ABORT"))
 			fserror = 1;
 
 		if (!strcmp(n, "FS_NAME"))
