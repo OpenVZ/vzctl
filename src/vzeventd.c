19affab511e819f397cfa3dfbb98b7f79a24ba36
Igor Sukhih <igor@virtuozzo.com>
2017-07-26 15:03:27 +0300

    Fixed compilation
    vzeventd.c:218:4: error: expected ‘)’ before ‘fserror’n
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- src/vzeventd.c
+++ src/vzeventd.c
@@ -214,7 +214,7 @@ static void process_uevent(void)
 		n = udev_list_entry_get_name(e);
 		v = udev_list_entry_get_value(e);
 		if (!strcmp(n, "FS_ACTION") && 
-				(!strcmp(v, "ABORT") || !strcmp(v, "ERROR"))
+				(!strcmp(v, "ABORT") || !strcmp(v, "ERROR")))
 			fserror = 1;
 
 		if (!strcmp(n, "FS_NAME"))
