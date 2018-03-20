OpenVZ/vzctl

commit 0c45c1286e80f994465c87375a56cea60a9f77bb

Add --ostemplate support for vzctl set

src/vzctl.c
@@ -84,6 +84,7 @@ static struct option set_options[] =
 	{"apply_iponly", required_argument, NULL, PARAM_APPLY_IPONLY},
 		{"apply-iponly", required_argument, NULL, PARAM_APPLY_IPONLY},
 	{"setmode",	required_argument, NULL, PARAM_SETMODE},
+	{"ostemplate", required_argument, NULL, PARAM_OSTEMPLATE},
 
 /* --device-add hdd --size <N> --type <expanded|raw> --image <path> */
 	{"device-add", required_argument, NULL, PARAM_DEVICE_ADD},
@@ -1126,6 +1127,9 @@ int ParseSetOptions(ctid_t ctid, struct CParam *param, int argc, char **argv)
 		case PARAM_ENC_KEYID:
 		case PARAM_DISK_OFFLINE:
 		case PARAM_DISK_STORAGE_URL:
+		case PARAM_OSTEMPLATE:
+			param->ostmpl = strdup(optarg);
+			break;
 		case PARAM_DISK_DETACH:
 			ret = add_disk_param(&tmpparam, &disk, c, optarg);
 			if (ret)

include/vzctl.h
@@ -217,7 +217,7 @@
 /* Create VZFS private area options */
 #define PARAM_PKGVER		11
 #define PARAM_PKGSET		12
-#define PARAM_OSTEMPLATE	13
+#define PARAM_OSTEMPLATE    VZCTL_PARAM_OSTEMPLATE
 
 #define PARAM_CPUWEIGHT		VZCTL_PARAM_CPUWEIGHT
 #define PARAM_CPULIMIT		VZCTL_PARAM_CPULIMIT

src/vz.c
@@ -429,7 +429,7 @@ fprintf(stdout,
 "   [--netfilter <disabled|stateless|stateful|full>]\n"
 "   [--iolimit <bytes/sec>] [--iopslimit <op/sec>] [--features name:on|off]\n"
 "   [--mount_opts <opt[,opt]>] [--ha_enable <yes|no>] [--ha_prio <N>]\n"
-"   [UBC parameters]\n"
+"   [UBC parameters] [--ostemplate <name>]\n"
 "UBC parameters (N - items, P - pages, B - bytes):\n"
 "Two numbers divided by colon denote barrier:limit.\n"
 "In case the limit is not given it is set to the same value as the barrier.\n"

commit 806bf477a9d0b36ccda6dcc24418d6c6f52d9947

non-option ARGV-elements: --device-uuid  #PSBM-82602
broken by commit 0c45c128

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

src/vzctl.c
@@ -1127,9 +1127,6 @@ int ParseSetOptions(ctid_t ctid, struct CParam *param, int argc, char **argv)
 		case PARAM_ENC_KEYID:
 		case PARAM_DISK_OFFLINE:
 		case PARAM_DISK_STORAGE_URL:
-		case PARAM_OSTEMPLATE:
-			param->ostmpl = strdup(optarg);
-			break;
 		case PARAM_DISK_DETACH:
 			ret = add_disk_param(&tmpparam, &disk, c, optarg);
 			if (ret)